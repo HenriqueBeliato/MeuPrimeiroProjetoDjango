b0f42ad7e059b287ef8d9d17249c827f64ca93cf
henrique.beliato <rick_beliato@yahoo.com.br>
2022-07-11 20:33:42 -0300

    Versão inicial do projeto

---
+++ Rh/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+from Rh.models import Departamento, Funcionario
+
+# Register your models here.
+admin.site.register(Funcionario)
+admin.site.register(Departamento)
